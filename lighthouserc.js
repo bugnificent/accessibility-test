c401c1ccbb7edaefb13e00783397c240be2c3363
Yusuf <38029093+bugnificent@users.noreply.github.com>
2025-03-20 19:10:21 +0300

    Update lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,6 +2,16 @@ module.exports = {
   ci: {
     collect: {
       staticDistDir: './',
+      "numberOfRuns": 1,
+      "url": ["https://yusufasik.com"],
+      "settings": {
+        "throttlingMethod": "devtools",
+        "emulatedFormFactor": "desktop",
+        "screenEmulation": { "mobile": false },
+        "disableStorageReset": true,
+        "waitForNetworkIdle": true,  // Add this to ensure the page waits for network idle
+        "maxWaitForFcp": 60000,  // Increase this to allow more time for the page to load
+        "maxWaitForLoad": 120000
     },
     upload: {
       target: 'temporary-public-storage',
