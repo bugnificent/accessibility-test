d4cd13d03e316925959db0deee82a75a90ff4582
Yusuf <38029093+bugnificent@users.noreply.github.com>
2025-03-21 14:43:11 +0300

    Update lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,8 +2,7 @@ module.exports = {
   ci: {
     collect: {
       staticDistDir: './',
-      exclude: ['**/access-report-axe.html'],// Replace with the path to your built files
-    },
+     },
     upload: {
       target: 'temporary-public-storage', // Use public storage
     },
